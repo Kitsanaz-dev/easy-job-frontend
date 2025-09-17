806dd9935e4343fad06dfec956a8205718ad7de9
Oh-oh12 <okayx4605@gmail.com>
2025-09-17 22:28:27 +0700

    Front first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes,Route } from "react-router-dom";
+import RegisterPage from "./page/RegisterPage";
+import LoginPage from "./page/LoginPage";
+import HomePage from "./page/HomePage";
+import CreatePostPage from "./page/CreatePostPage";
+import PostDatial from "./page/PostDatial";
+import React from 'react'
 
+<<<<<<< Updated upstream
 function App() {
   
 
+=======
+const App = () => {
+>>>>>>> Stashed changes
   return (
-    <>
-      <h1 className='text-xl text-blue-800'></h1>
-    </>
+     <div className="relative h-full w-full">
+      <div className=""/>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/create-post" element={<CreatePostPage />} />
+        <Route path="/post/:id" element={<PostDatial />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/services/auth.js
@@ -0,0 +1,34 @@
+import api from "./api";
+import { jwtDecode } from "jwt-decode";
+
+
+export const register = async (userData) => {
+    try {
+       const res = await api.post('/users',userData);
+       return res.data; 
+    } catch (error) {
+        return {error: error.response ? error.response.data : "Network Error"};
+    }
+}
+
+export const login = async (credentials) => {
+    try {
+        const res = await api.post("/users/login",credentials);
+        localStorage.setItem('token', res.data.token);
+        return res.data;
+    } catch (error) {
+        return {error: error.response ? error.response.data : "Network Error"};
+    }
+}
+
+export const getUserInfo = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) return null;
+    try {
+        const decoded = jwtDecode(token);
+        const res = await api.get(`/users/${decoded.userID}`);
+        return res.data;
+    } catch (error) {
+        return {error: error.response ? error.response.data : "Network Error"};
+    }
+}
